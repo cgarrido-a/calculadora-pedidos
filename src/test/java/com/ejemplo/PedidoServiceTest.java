ad4408c278877e66c51e53a932ea4db92c75cb6e
consuelo <consuelogarrido.ab@gmail.com>
2025-06-23 22:00:35 -0400

    first commit

---
+++ src/test/java/com/ejemplo/PedidoServiceTest.java
@@ -0,0 +1,34 @@
+package com.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PedidoServiceTest {
+
+    DescuentoRepository repository = new DescuentoRepository();
+    PedidoService service = new PedidoService(repository);
+
+    @Test
+    void testSinDescuentoYEnvioNormal() {
+        double total = service.calcularTotal(100, "", false);
+        assertEquals(110.0, total);
+    }
+
+    @Test
+    void testConDescuentoYEnvioExpress() {
+        double total = service.calcularTotal(100, "PROMO10", true);
+        assertEquals(110.0, total); // 100 - 10% + 20 = 110
+    }
+
+    @Test
+    void testConDescuentoYEnvioNormal() {
+        double total = service.calcularTotal(200, "PROMO10", false);
+        assertEquals(190.0, total);
+    }
+
+    @Test
+    void testSinDescuentoYEnvioExpress() {
+        double total = service.calcularTotal(150, "", true);
+        assertEquals(170.0, total);
+    }
+}
